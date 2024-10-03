61c53367e7ce95e9456aad32cee890c36e9341a9
Karoly1996 <futyi1996@hotmail.com>
2024-10-02 21:05:50 -0400

    reversedLL in python

--- Easy/ReverseLinkedList/ReverseLL.rb
+++ Easy/ReverseLinkedList/ReverseLL.rb
@@ -1,13 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
-# @param {ListNode} head
-# @return {ListNode}
 def reverse_list(head)
   previous = nil
   current = head
